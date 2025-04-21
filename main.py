2629094c4fd170afc49bed8efaae6600d24313e9
rodgerswisdom <trulyhawona@gmail.com>
2025-04-21 23:25:39 +0300

    Automated commit

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from app.api import webhook_handler
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Status":"OK"}
 
-app.include_router(webhook_handler.router, prefix="/webhook")
+app.include_router(webhook_handler.router, prefix="", tags=["webhook"])
+
