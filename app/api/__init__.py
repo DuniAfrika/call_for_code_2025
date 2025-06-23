5ed087f2a188758b3587275ca3a2c5c94662f537
rodgerswisdom <trulyhawona@gmail.com>
2025-06-24 00:53:45 +0300

    Automated commit

--- app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Create global imports for the API module.
-
-This module imports all the necessary
-components for the API module.
-"""
-
-from .whatsapp_utils import send_whatsapp_message, verify_webhook, process_whatsapp_message
--- app/services/__init__.py
+++
@@ -1,2 +0,0 @@
-from .nlp_processing import process_nlp
-from .image_processing import process_image
